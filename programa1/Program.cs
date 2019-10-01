fccc77e8d7c1f73bba5457c5a3441b5384f4d930
0mariaragos <mariana.aragos@etec.sp.gov.br>
2019-09-30 22:07:31 -0300

    primeiro commit c#

---
+++ programa1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            Console.Write("Digite seu nome: " );
+            nome = Console.ReadLine();
+        }
+    }
+}
