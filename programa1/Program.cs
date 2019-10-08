61aed560a91aa5dbc8f3521cd66edd5cdace3e7e
0mariaragos <mariana.aragos@etec.sp.gov.br>
2019-10-07 22:38:43 -0300

    terceiro commit

--- programa1/Program.cs
+++ programa1/Program.cs
@@ -10,12 +10,14 @@ namespace programa1
     {
         static void Main(string[] args)
         {
-            string nome;
+            string nome, pet;
             int idade;
             Console.Write("Digite seu nome: " );
             nome = Console.ReadLine();
             Console.Write("Digite sua idade: ");
             idade = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do seu animal de estimação: ");
+            pet = Console.ReadLine();
         }
     }
 }
